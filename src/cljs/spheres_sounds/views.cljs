fc59be949dc445c9ba0faac12e3bd35586df8e8f
manandearth <adam@manandearth.net>
2019-01-26 23:23:57 +0100

    appropriate representations of the planets in the selected-system-box

--- src/cljs/spheres_sounds/views.cljs
+++ src/cljs/spheres_sounds/views.cljs
@@ -8,88 +8,99 @@
 
 (defonce context (cljs-bach.synthesis/audio-context))
 
+(def x-position 100)
 
-(defn systems-box []
-  (let [x-position 100]
-    [:svg.system {:x 0
-                  :y 0
-                  :height 120
-                  :width 1100}
-     [:defs
-      [:filter
-       ;;the filter size here defines the bounding box in which the effect will take place.
-       {:id "f2" :x "-100%" :y "-100%" :width "300%" :height "300%"}
-       [:feOffset
-        {:result "offOut" :in "SourceGraphic" :dx "0" :dy "0"}]
-       [:feGaussianBlur
-        {:result "blurOut" :in "offOut" :stdDeviation "8"}]
-       [:feBlend
-        {:in "SourceGraphic" :in2 "blurOut" :mode "normal"}]]
 
-      [:filter
-       ;;the filter size here defines the bounding box in which the effect will take place.
-       {:id "f3" :x "-100%" :y "-100%" :width "300%" :height "300%"}
-       [:feOffset
-        {:result "offOut" :in "SourceGraphic" :dx "0" :dy "0"}]
-       [:feGaussianBlur
-        {:result "blurOut" :in "offOut" :stdDeviation "2"}]
-       [:feBlend
-        {:in "SourceGraphic" :in2 "blurOut" :mode "normal"}]]
+(defn defs []
+  [:svg [:defs
+         [:filter
+          ;;the filter size here defines the bounding box in which the effect will take place.
+          {:id "f2" :x "-100%" :y "-100%" :width "300%" :height "300%"}
+          [:feOffset
+           {:result "offOut" :in "SourceGraphic" :dx "0" :dy "0"}]
+          [:feGaussianBlur
+           {:result "blurOut" :in "offOut" :stdDeviation "8"}]
+          [:feBlend
+           {:in "SourceGraphic" :in2 "blurOut" :mode "normal"}]]
 
-      ;;mask for sun
-      [:mask {:id "m1" :x "0" :y "0" :width "200%" :height "100%"}
-       [:rect {:x x-position :width "1000" :height "100" :fill "white"}]]
-      ;;mask for jupiter
-      [:mask {:id "m-jupiter" :x "0" :y "0" :width "200%" :height "100%"}
-       [:circle {:cx (+ x-position 500) :cy 50 :r 42 :fill "white"}]]
-      ;;mask for earth
-      [:mask {:id "m-earth" :x "0" :y "0" :width "200%" :height "100%"}
-       [:circle {:cx (+ x-position 240) :cy 50 :r 12 :fill "white"}]]
-      ;;masks for saturn
+         [:filter
+          ;;the filter size here defines the bounding box in which the effect will take place.
+          {:id "f3" :x "-100%" :y "-100%" :width "300%" :height "300%"}
+          [:feOffset
+           {:result "offOut" :in "SourceGraphic" :dx "0" :dy "0"}]
+          [:feGaussianBlur
+           {:result "blurOut" :in "offOut" :stdDeviation "2"}]
+          [:feBlend
+           {:in "SourceGraphic" :in2 "blurOut" :mode "normal"}]]
 
-      [:mask {:id "m-saturn"}
-       ;;TODO need to reverse the mask on Saturn
-       [:rect {:x (+ x-position 580) :y 0 :width 200 :height 200 :fill "white"}]
-       [:circle {:cx (+ x-position 650) :cy 50 :r 28 :fill "black"}]
-       [:rect {:x (+ x-position 580) :y 50 :width 200 :height 30 :fill "white"}]
-       ]
-      [:mask {:id "m-uranus"}
-       ;;TODO need to reverse the mask on Saturn
-       [:rect {:x (+ x-position 700) :y 0 :width 200 :height 200 :fill "white"}]
-       [:circle {:cx (+ x-position 760) :cy 50 :r 20 :fill "black"}]
-       [:rect {:x (+ x-position 700) :y 50 :width 200 :height 30 :fill "white"}]
-       ]
+         ;;mask for sun
+         ;; [:mask {:id "m1" :x "0" :y "0" :width "200%" :height "100%"}
+         ;;  [:rect {:x x-position :width "1000" :height "100" :fill "white"}]]
+         ;;mask for jupiter
+         [:mask {:id "m-jupiter" :x "0" :y "0" :width "200%" :height "100%"}
+          [:circle {:cx (+ x-position 500) :cy 50 :r 42 :fill "white"}]]
+         ;;mask for earth
+         [:mask {:id "m-earth" :x "0" :y "0" :width "200%" :height "100%"}
+          [:circle {:cx (+ x-position 240) :cy 50 :r 12 :fill "white"}]]
+         ;;masks for saturn
 
-      
-      [:linearGradient {:id "saturn-gradient" :x (+ x-position 25) :y 480 :width 400 :height 400 :gradientTransform "rotate(90, 0, 0)" }
-       [:stop {:offset "0%"   :stop-color "rgba(255, 255, 255, 0)"}]
-       [:stop {:offset "100%" :stop-color "rgba(255, 255, 255, 255)"}]]
+         [:mask {:id "m-saturn"}
+          ;;TODO need to reverse the mask on Saturn
+          [:rect {:x (+ x-position 580) :y 0 :width 200 :height 200 :fill "white"}]
+          [:circle {:cx (+ x-position 650) :cy 50 :r 28 :fill "black"}]
+          [:rect {:x (+ x-position 580) :y 50 :width 200 :height 30 :fill "white"}]
+          ]
+         [:mask {:id "m-uranus"}
+          ;;TODO need to reverse the mask on Saturn
+          [:rect {:x (+ x-position 700) :y 0 :width 200 :height 200 :fill "white"}]
+          [:circle {:cx (+ x-position 760) :cy 50 :r 20 :fill "black"}]
+          [:rect {:x (+ x-position 700) :y 50 :width 200 :height 30 :fill "white"}]
+          ]
 
-      ]
-     [:rect.system {:x x-position
-                    :y 2
-                    :width 1000
-                    :height 100
-                    }]
-     [:circle.sphere.sun {:cx (+ x-position -120)
-                          :cy 50
-                          :r 200
-                          :filter "url(#f2)"
-                          :mask "url(#m1)"
-                          }
-      ]
-     [:circle.sphere.mercury {:cx (+ x-position 150) :cy 50 :r 3 :filter "url(#f2)"}]
-     [:circle.sphere.venus {:cx (+ x-position 190) :cy 50 :r 9 :filter "url(#f2)"}]
-     [:g
+         
+         [:linearGradient {:id "saturn-gradient" :x (+ x-position 25) :y 480 :width 400 :height 400 :gradientTransform "rotate(90, 0, 0)" }
+          [:stop {:offset "0%"   :stop-color "rgba(255, 255, 255, 0)"}]
+          [:stop {:offset "100%" :stop-color "rgba(255, 255, 255, 255)"}]]
+
+         ]])
+
+(defn sun []
+  [:svg
+   ;; [:defs
+   ;;       ;;mask for sun
+   ;;       [:mask {:id "m1" :x "0" :y "0" :width "200%" :height "100%"}
+   ;;        [:rect {:x x-position :width "1000" :height "100" :fill "white"}]]]
+   ;[defs]
+   [:circle.sphere.sun {:cx (+ x-position -120)
+                        :cy 50
+                        :r 200
+                        :filter "url(#f2)"
+                        :mask "url(#m1)"
+                        }
+    ]])
+(defn mercury []
+  [:svg
+   [:circle.sphere.mercury {:cx (+ x-position 150) :cy 50 :r 3 :filter "url(#f2)"}]])
+
+
+(defn venus []
+  [:svg
+   [:circle.sphere.venus {:cx (+ x-position 190) :cy 50 :r 9 :filter "url(#f2)"}]])
+
+(defn earth []
+  [:g {:title "Hello-world!"}
       [:circle.sphere.earth {:cx (+ x-position 240) :cy 50 :r 12 :filter "url(#f2)"
                              :on-click #(js/alert "earth")}]
       [:ellipse {:cx (+ x-position 250) :cy 45 :rx 10 :ry 7 :filter "url(#f2)" :mask "url(#m-earth)" :style {:fill "#3a4"}}]
-      [:ellipse {:cx (+ x-position 230) :cy 55 :rx 10 :ry 7 :filter "url(#f2)" :mask "url(#m-earth)" :style {:fill "#3a4"}}]]
-     [:g
+      [:ellipse {:cx (+ x-position 230) :cy 55 :rx 10 :ry 7 :filter "url(#f2)" :mask "url(#m-earth)" :style {:fill "#3a4"}}]])
+
+(defn mars []
+  [:g
       [:circle.sphere.mars {:cx (+ x-position 300) :cy 50 :r 7 :filter "url(#f2)"}]
       [:circle.sphere {:cx (+ x-position 300) :cy 50 :r 4 :style {:fill "#b66"} :filter "url(#f3)"}]
-      ]
-     [:g 
+      ])
+(defn jupiter []
+  [:g 
       [:circle.sphere.jupiter {:cx (+ x-position 500) :cy 50 :r 42 :filter "url(#f2)"}]
       [:g {:mask "url(#m-jupiter)"}
        [:circle {:cx (+ x-position 500) :cy -100 :r 120:stroke "#bbb" :stroke-width "7px" :fill "none"}]
@@ -100,9 +111,10 @@
        [:circle {:cx (+ x-position 500) :cy -100 :r 172 :stroke "#f55" :stroke-width "7px" :fill "none"}]
        [:circle {:cx (+ x-position 500) :cy -100 :r 188 :stroke "#f88" :stroke-width "7px" :fill "none"}]
        [:circle {:cx (+ x-position 500) :cy -100 :r 200 :stroke "#bbb" :stroke-width "7px" :fill "none"}]
-       [:ellipse {:cx (+ x-position 520) :cy 65 :rx 10 :ry 6 :style {:fill "#b53" :stroke "#bbb" :stroke-width "2px"}}]]]
+       [:ellipse {:cx (+ x-position 520) :cy 65 :rx 10 :ry 6 :style {:fill "#b53" :stroke "#bbb" :stroke-width "2px"}}]]])
 
-     [:g {:transform "translate (80 -300) rotate(25 20 0)"} 
+(defn saturn []
+  [:g {:transform "translate (80 -300) rotate(25 20 0)"} 
       [:circle.sphere.saturn {:cx (+ x-position 650) :cy 50 :r 28 :filter "url(#f2)"}]
       [:svg {:height 1000 
              ;; :filter "url(#f2)"
@@ -137,17 +149,52 @@
                   :stroke-width 3 :fill "none" :opacity "1" :mask "url(#m-saturn)"
                    :filter "url(#f3)" 
                    }]]]
-     [:g
+     )
+
+(defn uranus []
+  [:g
       [:circle.sphere.uranus {:cx (+ x-position 760) :cy 50 :r 20 :filter "url(#f2)"}]
       [:ellipse {:cx (+ x-position 760) :cy 50 :rx 30 :ry 10 :fill "none" :stroke-width "1px" :stroke "white" :opacity "0.3"
                  :transform "rotate(100,860,50)" :mask "url(#m-uranus)"}]
       [:ellipse {:cx (+ x-position 760) :cy 50 :rx 30 :ry 10 :fill "none" :stroke-width "1px" :stroke "white" :opacity "0.5"
                  :transform "rotate(120,860,50)" :mask "url(#m-uranus)"}]]
 
-     [:g
+     )
+
+(defn neptune []
+  [:g
       [:circle.sphere.neptune {:cx (+ x-position 860) :cy 50 :r 18 :filter "url(#f2)"}]
       [:ellipse {:cx (+ x-position 865) :cy 48 :rx 6 :ry 3 :filter "url(#f3)" :style {:fill "#33f"}}]]
-     [:circle.sphere.pluto {:cx (+ x-position 940) :cy 50 :r 5 :filter "url(#f2)"}]
+     )
+
+(defn pluto []
+  [:circle.sphere.pluto {:cx (+ x-position 940) :cy 50 :r 5 :filter "url(#f2)"}]
+     
+     )
+
+(defn systems-box []
+  (let [x-position 100]
+    [:svg.system {:x 0
+                  :y 0
+                  :height 120
+                  :width 1100}
+     [defs]
+
+     [:rect.system {:x x-position
+                    :y 2
+                    :width 1000
+                    :height 100
+                    }]
+     [sun]
+     [mercury]
+     [venus]
+     [earth]
+     [mars]
+     [jupiter]
+     [saturn]
+     [uranus]
+     [neptune]
+     [pluto]
      
      ]))
 
@@ -184,16 +231,8 @@
            ]))])]
   )
 
-(defn selected-system-box []
-  (let [x-position 100
-        parent (subscribe [::subs/selected-system-attr])
-        spheres (subscribe [::subs/sorted-spheres])
-        ]
-    [:svg.system {:x 0
-                  :y 100
-                  :height 120
-                  :width 1100}
-     [:defs
+(defn defs2 []
+  [:svg [:defs
       [:filter
        ;;the filter size here defines the bounding box in which the effect will take place.
        {:id "f2" :x "-100%" :y "-100%" :width "300%" :height "300%"}
@@ -206,35 +245,42 @@
       
       [:mask {:id "m1" :x "0" :y "0" :width "200%" :height "100%"}
        [:rect {:x x-position :width "1000" :height "100" :fill "white"}]]
-      ]
-     [:rect.system {:x x-position
+         ]])
+
+(defn selected-system-box []
+  (let [x-position 100
+        parent (subscribe [::subs/selected-system-attr])
+        spheres (subscribe [::subs/sorted-spheres])
+        ]
+    
+    [:svg.system {:x 100
+                  :y 100
+                  :height 104
+                  :width 1100}
+     [defs2]
+     [:rect.system {:x 0
                     :y 2
                     :width 1000
                     :height 100
                     }]
-     [:circle.sphere {:cx (+ 100 (/ (.log js/Math (:volume @parent)) 2 ))
-                      :cy  50
-                      ;;TODO I'm here with the pow!
-                      :r  (/ (.pow js/Math (.log10 js/Math (:volume @parent))3) 100) 
-                      :fill (case (:name @parent)
-                              "Sun" "#ff6"
-                              "Earth" "#19d"
-                              "Mars" "#833"
-                              "Jupiter" "#943"
-                              "Saturn" "#a81"
-                              "Uranus" "#19d"
-                              "Neptune" "#17d"
-                              "Pluto" "#533")
-                      :filter "url(#f2)"
-                      :mask "url(#m1)"}
-      ]
+     
+     [:svg
+      (case (:name @parent)
+        "Sun" [:svg {:x -100} [sun]]
+        "Earth" [:g {:transform "translate (-2050 -250) scale (6 6)"} [earth]]
+        "Mars" [:g {:transform "translate (-2800 -300) scale (7 7)"} [mars]]
+        "Jupiter" [:g {:transform "translate (-3150 -250) scale (5 5)"} [jupiter]]
+        "Saturn" [:g {:transform "translate (-3800 -250) scale (5 5)"} [saturn]]
+        "Uranus" [:g {:transform "translate (-4300 -280) scale (5 5)"} [uranus]]
+        "Neptune" [:g {:transform "translate (-4800 -180) scale (5 5)"} [neptune]]
+        "Pluto" [:g {:transform "translate (-8330 -350) scale (8 8)"} [pluto]])]
      (let [apo-list (map #(.log js/Math (/ (+ (:periapsis %) (:apoapsis %)) 2)) @spheres)
            apo-max (apply max (rest apo-list))
            apo-min (apply min (rest apo-list))
            apo-average  (/ (+ apo-max apo-min) 2)]
   (for [sphere (rest @spheres)]
     [:circle.satelite {:key (str "circle " sphere)
-                       :cx (+ 20 (/ (.log js/Math (:apoapsis sphere)) (/ apo-max 1000)))
+                       :cx (+ 20 (/ (.log js/Math (:apoapsis sphere)) (/ apo-max 900)))
                        :cy 50
                        :r 3
                        :filter "url(#f2"}]))
@@ -360,15 +406,15 @@
                     :height 1000
                     :position "absolute"}}
      [:div.guide
-      [:h1  @name "/interplanetary instrument"]
-[:h3 "Some of the bodies in our solar system are gigatic and they travel in greater speed than anything on this planet, yet the are silent. The following is an interactive exploration of the relation between some of those bodies through sound."]
+      [:h1 {:title "hey"} @name "/interplanetary instrument"]
+[:h3 "Some of the bodies in our solar system are gigatic and they travel in greater speed than anything on this planet, yet they are silent. The following is an interactive exploration of the relation between some of those bodies through sound."]
       [:h2 "Select a system:"]]
      [systems-menu]
      [systems-box]
-     [:h2.guide "Toggle spheres on and off:"]
+     [:h2.guide "Toggle spheres on or off:"]
      [stage]
      [:h2.guide "Choose the attribute that will be used for frequency:"]
-     [:h3.guide "This range will be set globally -> 30Hz the lowest and 12KHz the highest"]
+     [:h3.guide {:title "you will need headphones or some reasonable speakers as the tones tend to mostly be right at the edges.."} "This range will be set globally -> 30Hz the lowest and 12KHz the highest"]
      [player]
      [volume-slider]
      [footer]
